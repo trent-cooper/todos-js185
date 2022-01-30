ea4739649724a60f3a6c0316a1ce5ae89e5ed4c5
trent-cooper <00trent@protonmail.com>
2022-01-29 19:34:55 -0600

    Initial commit

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+  username text PRIMARY KEY,
+  password text NOT NULL
+);
+
+CREATE TABLE todolists (
+  id serial PRIMARY KEY,
+  title text NOT NULL UNIQUE,
+  username text NOT NULL REFERENCES users(username) ON DELETE CASCADE
+);
+
+CREATE TABLE todos (
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  done boolean NOT NULL DEFAULT false,
+  todolist_id integer NOT NULL REFERENCES todolists(id) ON DELETE CASCADE,
+  username text NOT NULL REFERENCES users(username) ON DELETE CASCADE
+);
+
